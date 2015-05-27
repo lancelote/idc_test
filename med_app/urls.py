53e3973bef74fa171e700d7b5213cd2a4d86a428
lancelote <lancelote.du.lac@gmail.com>
2015-05-27 22:57:24 +0300

    patient_detail view, template and url

--- med_app/urls.py
+++ med_app/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.patient_list),
+    url(r'^patient/(?P<pk>[0-9]+)/$', views.patient_detail),
 ]
--- med_app/views.py
+++ med_app/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from .models import Patient
+from django.shortcuts import render, get_object_or_404
+from .models import Patient, Document, RegAddress, ActAddress
 
 
 def patient_list(request):
     patients = Patient.objects.order_by('id')
     return render(request, 'med_app/patient_list.html', {'patients': patients})
+
+
+def patient_detail(request, pk):
+    patient = get_object_or_404(Patient, pk=pk)
+    document = Document.objects.get(patient_id=patient.id)
+    reg_address = RegAddress.objects.get(patient_id=patient.id)
+    act_address = ActAddress.objects.get(patient_id=patient.id)
+    return render(request, 'med_app/patient_detail.html',
+                  {'patient': patient,
+                   'document': document,
+                   'reg_address': reg_address,
+                   'act_address': act_address})

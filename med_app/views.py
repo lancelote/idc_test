5bf3afddc80ed74d57607c921106e3f53b017e34
lancelote <lancelote.du.lac@gmail.com>
2015-05-27 21:30:53 +0300

    update views and template

--- med_app/views.py
+++ med_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Patient
 
 
 def patient_list(request):
-    return render(request, 'med_app/patient_list.html', {})
+    patients = Patient.objects.order_by('id')
+    return render(request, 'med_app/patient_list.html', {'patients': patients})

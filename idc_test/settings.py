86b0728ab0260c7e6f587d32602c9cb3b915e92a
lancelote <lancelote.du.lac@gmail.com>
2015-05-27 22:03:46 +0300

    bootstrap

--- idc_test/settings.py
+++ idc_test/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'med_app/static')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'med_app/templates'),

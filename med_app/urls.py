3960ae5a0df18a2468399c5dc3c6984013900493
lancelote <lancelote.du.lac@gmail.com>
2015-05-28 20:58:54 +0300

    edit functionality

--- med_app/urls.py
+++ med_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.patient_list),
     url(r'^patient/(?P<pk>[0-9]+)/$', views.patient_detail),
     url(r'^patient/new/$', views.patient_new, name='patient_new'),
+    url(r'^patient/(?P<pk>[0-9]+)/edit/$', views.patient_edit,
+        name='patient_edit'),
 ]
--- med_app/views.py
+++ med_app/views.py
@@ -59,3 +59,51 @@ def patient_new(request):
                    'document_form': document_form,
                    'reg_address_form': reg_address_form,
                    'act_address_form': act_address_form})
+
+
+def patient_edit(request, pk):
+    patient = get_object_or_404(Patient, pk=pk)
+    document = Document.objects.get(patient_id=patient.id)
+    reg_address = RegAddress.objects.get(patient_id=patient.id)
+    act_address = ActAddress.objects.get(patient_id=patient.id)
+
+    if request.method == "POST":
+        patient_form = PatientForm(request.POST, instance=patient)
+        document_form = DocumentForm(request.POST, instance=document)
+        reg_address_form = RegAddressForm(request.POST, prefix='reg',
+                                          instance=reg_address)
+        act_address_form = ActAddressForm(request.POST, prefix='act',
+                                          instance=act_address)
+
+        if all((
+            patient_form.is_valid(),
+            document_form.is_valid(),
+            reg_address_form.is_valid(),
+            act_address_form.is_valid(),
+        )):
+            patient = patient_form.save(commit=False)
+            patient.save()
+
+            document = document_form.save(commit=False)
+            document.patient = patient
+            document.save()
+
+            reg_address = reg_address_form.save(commit=False)
+            reg_address.patient = patient
+            reg_address.save()
+
+            act_address = act_address_form.save(commit=False)
+            act_address.patient = patient
+            act_address.save()
+
+            return redirect('med_app.views.patient_detail', pk=patient.pk)
+    else:
+        patient_form = PatientForm(instance=patient)
+        document_form = DocumentForm(instance=document)
+        reg_address_form = RegAddressForm(prefix='reg', instance=reg_address)
+        act_address_form = ActAddressForm(prefix='act', instance=act_address)
+    return render(request, 'med_app/patient_edit.html',
+                  {'patient_form': patient_form,
+                   'document_form': document_form,
+                   'reg_address_form': reg_address_form,
+                   'act_address_form': act_address_form})

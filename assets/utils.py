c3699bc74f96009077adcb00953bb9bfe4f2663b
lancelote <lancelote.du.lac@gmail.com>
2015-06-10 19:50:03 +0300

    security fixes

---
+++ assets/utils.py
@@ -0,0 +1,20 @@
+from django.utils.crypto import get_random_string
+
+
+def generate_local_settings(file):
+    """
+    Generate local_settings file for deployment
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    secret_key = get_random_string(50, chars)
+
+    settings = (
+        'SECRET_KEY = "{0}"'.format(secret_key),
+        'DEBUG = False',
+        'ALLOWED_HOSTS = ["idctest.pythonanywhere.com"]',
+        'ADMINS = (("Pavel", "karateev.pavel@ya.ru"), )'
+    )
+
+    with open(file, 'w+') as local_settings:
+        local_settings.write('"""\n Production settings\n """\n')
+        local_settings.write('\n'.join(settings) + '\n')
